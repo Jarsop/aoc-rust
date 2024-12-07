abc733df6f4b59abdd32680c3e092866cf309a41
Jean-Pierre Geslin <jarsop@outlook.com>
2024-12-07 14:28:07 +0100

    day01 2024 part2
    Signed-off-by: Jean-Pierre Geslin <jarsop@outlook.com>

--- src/aoc2024/day01.rs
+++ src/aoc2024/day01.rs
@@ -1,22 +1,28 @@
+use std::collections::HashMap;
+
 use crate::utils::solver;
 
 pub struct Solver;
+
+fn convert_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut nums = line
+                .split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .take(2);
+            (nums.next().unwrap(), nums.next().unwrap())
+        })
+        .unzip()
+}
 
 impl solver::Solver<2024, 1> for Solver {
     type Part1 = i32;
     type Part2 = i32;
 
     fn solve_part_one(&self, input: &str) -> Self::Part1 {
-        let mut cols: (Vec<i32>, Vec<i32>) = input
-            .lines()
-            .map(|line| {
-                let mut nums = line
-                    .split_whitespace()
-                    .map(|n| n.parse::<i32>().unwrap())
-                    .take(2);
-                (nums.next().unwrap(), nums.next().unwrap())
-            })
-            .unzip();
+        let mut cols = convert_input(input);
 
         cols.0.sort();
         cols.1.sort();
@@ -29,6 +35,20 @@ impl solver::Solver<2024, 1> for Solver {
     }
 
     fn solve_part_two(&self, input: &str) -> Self::Part2 {
-        42
+        let cols = convert_input(input);
+        let mut similarity_scores = HashMap::new();
+
+        for (i, a) in cols.0.iter().enumerate() {
+            for b in &cols.1 {
+                if a == b {
+                    *similarity_scores.entry((i, a)).or_insert(0) += 1;
+                }
+            }
+        }
+
+        similarity_scores
+            .iter()
+            .map(|((_, n), similarity)| **n * similarity)
+            .sum::<i32>()
     }
 }
